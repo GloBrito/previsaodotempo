6231737760e276532e6a198e75b962349b626a94
Glória Brito <gloriafbrito@gmail.com>
2023-08-30 21:08:56 -0300

    adciona bib requests

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
+import requests
 import tkinter as tk
 from tkinter import messagebox
 
-API_KEY = '41086682a7303f3dfa5d1c44ea2fec98'
+api_key = '41086682a7303f3dfa5d1c44ea2fec98'
+
+def get_weather(city_name, api_key):
+    base_url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
+    params = {
+        "q": city_name,
+        "appid": api_key,
+        "units": "metric"  # Para obter a temperatura em Celsius
+    }
+    response = requests.get(base_url, params=params)
+    data = response.json()
+    return data
 
 def get_weather_button_click():
     city_name = city_entry.get()
-    weather_data = get_weather(city_name, API_KEY)
+    weather_data = get_weather(city_name, api_key)
 
     if weather_data.get("cod") == 200:
         temperature = weather_data["main"]["temp"]
